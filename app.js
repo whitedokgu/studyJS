e1206a041eee18665627766f8bb737630af7a275
Jaedong Sim <80078375+whitedokgu@users.noreply.github.com>
2022-05-10 11:35:55 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -20,7 +20,23 @@ function handleWindowResize() {
   document.body.style.backgroundColor = "tomato";
 }
 
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOOOOOOOOOOOD");
+}
+
 title.addEventListener("click", handleTitleClick);
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
+
 window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
